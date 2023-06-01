c47ccfab2162352858ccfef0938e4fb617cf6848
FDAlmaraz <135261153+FDAlmaraz@users.noreply.github.com>
2023-06-01 11:29:50 -0700

    Add files via upload

---
+++ sumanumeros/src/Main.java
@@ -0,0 +1,11 @@
+package suma.bootcamp;
+public class Main {
+    public static void main(String[] args) {
+        int resultado = sumar(1,2,3);
+        System.out.println(resultado);
+    }
+
+    public static int sumar(int num1, int num2, int num3){
+        return num1+num2+num3;
+    }
+}
